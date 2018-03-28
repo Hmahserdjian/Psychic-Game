4a3eeacc9da539e8eb204e18aeb3ba79b97a1e39
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-03-28 09:08:03 -0400

    i did something

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+// console.log ('log');
+
+// var random = Math.floor((Math.random() * 10) + 1);
+
+// console.log(random);
+// var user= prompt('guess a number from 1 - 10');
+
+// if (random == user)  {console.log("you win")
+
+    
+// } else {console.log("you lose")
+    
+// }
+//Created an array called Letters and a variable i to help with for loop starting number
+var letters =["a", "b", "c","d", "e", "f"];
+var i;
+//for loop going through each item in array and incrementing with i++
+for (i = 0; i <  letters.length; i++  ) {
+//show the results to the console log to see if all three show up, I added the letters variable and the i so it appears as a,b,c.
+    console.log(letters[i]);
+  }
