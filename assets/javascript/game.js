3492122900df5b18cca2991ef43a54786da8904c
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-03-28 11:48:14 -0400

    changes made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,8 +14,9 @@
 //Created an array called Letters and a variable i to help with for loop starting number
 var letters =["a", "b", "c","d", "e", "f"];
 var i;
-//for loop going through each item in array and incrementing with i++
-for (i = 0; i <  letters.length; i++  ) {
-//show the results to the console log to see if all three show up, I added the letters variable and the i so it appears as a,b,c.
+//
+for (i = 0; i <  letters.length; i++) {
+//
     console.log(letters[i]);
   }
+   var userText = document.getElementById("user-text");
