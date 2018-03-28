5589e90c76129eb67e6ee479d8b8b76914889807
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-03-28 11:51:45 -0400

    abcs complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,7 @@
     
 // }
 //Created an array called Letters and a variable i to help with for loop starting number
-var letters =["a", "b", "c","d", "e", "f"];
+var letters =["a", "b", "c","d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var i;
 //
 for (i = 0; i <  letters.length; i++) {
