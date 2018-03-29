d8c3640c75c68ee96054a1223024be8e2eebd32e
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-03-28 20:37:09 -0400

    i made changes java randomly picks a letter now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,17 +12,28 @@
     
 // }
 //Created an array called Letters and a variable i to help with for loop starting number
-var letters =["a", "b", "c","d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var userGuess =["a", "b", "c","d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var computerGuess =["a", "b", "c","d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var i;
-//
-for (i = 0; i <  letters.length; i++) {
-//
-    console.log(letters[i]);
-  }
-  var userGuess = document.getElementById("user-text");
-  var computerGuess = document.getElementById("computer guess");
+for (i = 0; i <  userGuess.length; i++);
+document.onkeyup = function(userGuess) {
+var userGuess = event.key;
+
+    console.log(userGuess[i]);
+  };
+
+  
+
+  var computerGuess = computerGuess[Math.floor(Math.random() * computerGuess.length)];
+console.log(computerGuess)
+  
   if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") ||
   (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m" ) || (userGuess === "n") ||
   (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") ||
   (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")); 
+
+function userGuess() {
+  document.getElementById("hello").innerHTML = "hello"
+};
+console.log()
 
