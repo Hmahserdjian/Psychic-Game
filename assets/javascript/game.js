ce2f22b48acd5b164a4875d9c946b4b13d465a50
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-03-29 09:53:04 -0400

    inital commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,34 +7,29 @@
 
 // if (random == user)  {console.log("you win")
 
-    
+
 // } else {console.log("you lose")
-    
+
 // }
 //Created an array called Letters and a variable i to help with for loop starting number
-var userGuess =["a", "b", "c","d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var computerGuess =["a", "b", "c","d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var i;
-for (i = 0; i <  userGuess.length; i++);
-document.onkeyup = function(userGuess) {
-var userGuess = event.key;
+var userGuess = [];
+var computerGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var userGuess = document.getElementById("userGuess");
+var computerGuess = computerGuess[Math.floor(Math.random() * computerGuess.length)];
 
-    console.log(userGuess[i]);
-  };
+document.onkeyup = function (event) {
+
+  userGuess.textContent = event.key;
+  console.log(userGuess);
+
 
   
-
-  var computerGuess = computerGuess[Math.floor(Math.random() * computerGuess.length)];
-console.log(computerGuess)
-  
-  if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") ||
-  (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m" ) || (userGuess === "n") ||
-  (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") ||
-  (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")); 
-
-function userGuess() {
  
+  checkGuesses();
+};
+  function checkGuesses() {
 
-}
-
-
+  if (userGuess == computerGuess) {
+      alert("you won");
+    }
+  }
